07b6b8d235c0eecc6c4ceacbba5e431d6779f16c
TheEnder146 <88066111+TheEnder146@users.noreply.github.com>
2023-12-10 13:40:52 -0600

    Create conexion_1.php

---
+++ conexion_1.php
@@ -0,0 +1,15 @@
+<?php 
+
+	$host = "localhost";
+	$usuario = "root";
+	$contraseña = "";
+	$BD = "proyecto_transversal";
+
+	$conexion = new mysqli_connect($localhost, $root, $contraseña $proyecto_transversal);
+
+
+	if ($conexionn->connect_error) {
+		die("error de conexion:  ", $conexion->connect_error);
+	}
+
+ ?>
